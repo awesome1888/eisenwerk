7760f90582e85db3cdc5d343c56e5a39e97547b8
Sergei <gannochenko.sv@gmail.com>
2018-11-04 17:07:42 +0100

    scripts

--- app.front.client/src/pages/Home/index.jsx
+++ app.front.client/src/pages/Home/index.jsx
@@ -8,7 +8,7 @@ class HomePage extends React.Component {
     render() {
         return (
             <Layout>
-                Hello there 6
+                Hello there 7
             </Layout>
         );
     }
